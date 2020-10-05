48b10a5034581126347cc8252b73cd2c3c60e783
Juranek <juranek.lukasz@gmail.com>
2020-10-05 22:29:14 +0200

    Add status request

--- firmware/Src/ucan_fd_protocol_stm32g431.c
+++ firmware/Src/ucan_fd_protocol_stm32g431.c
@@ -5,7 +5,6 @@
  *      Author: Admin
  */
 
-
 #include "ucan_fd_protocol_stm32g431.h"
 #include "stm32g4xx_hal_fdcan.h"
 #include "jump_to_boot.h"
@@ -16,7 +15,6 @@ extern FDCAN_HandleTypeDef hfdcan1;
 extern Ring_type usb_rx;
 extern Ring_type usb_tx;
 
-
 UCAN_AckFrameDef ack_frame = { UCAN_FD_ACK,	// frame_type
 		UCAN_FD_COMMAND_OK, //cmd status
 		{ }, //FDCAN_ProtocolStatusTypeDef
@@ -25,91 +23,89 @@ UCAN_AckFrameDef ack_frame = { UCAN_FD_ACK,	// frame_type
 		{ 2, 1, UCAN_CAN_FD, { 1, 2, 3, 4, 5 } } //FDCAN_Device_DescritionDef
 };
 
-uint32_t UCAN_get_frame_size(UCAN_FRAME_TYPE ucan_frame)
-{
-	static uint32_t sizeof_array[] = {
-			sizeof(UCAN_InitFrameDef), //UCAN_FD_INIT
-			sizeof(UCAN_DeinitFrameDef),//UCAN_FD_DEINIT
+uint32_t UCAN_get_frame_size(UCAN_FRAME_TYPE ucan_frame) {
+	static uint32_t sizeof_array[] = { sizeof(UCAN_InitFrameDef), //UCAN_FD_INIT
+			sizeof(UCAN_DeinitFrameDef), //UCAN_FD_DEINIT
 			sizeof(UCAN_TxFrameDef), //UCAN_FD_TX
 			sizeof(UCAN_SaveConfigFrameDef), //UCAN_FD_SAVE_CONFIG
-			sizeof(UCAN_GoToBootladerFrameDef),//UCAN_FD_GO_TO_BOOTLOADER
-			sizeof(UCAN_Get_CAN_Status),//UCAN_FD_GET_CAN_STATUS
+			sizeof(UCAN_GoToBootladerFrameDef), //UCAN_FD_GO_TO_BOOTLOADER
+			sizeof(UCAN_Get_CAN_Status), //UCAN_FD_GET_CAN_STATUS
 			sizeof(UCAN_RxFrameDef), //UCAN_FD_RX
 			sizeof(UCAN_AckFrameDef) //UCAN_FD_ACK
-	};
+			};
 
-	if ((uint32_t)ucan_frame < (uint32_t)UCAN_FD_LAST)
-		return sizeof_array[(uint32_t)ucan_frame];
+	if ((uint32_t) ucan_frame < (uint32_t) UCAN_FD_LAST)
+		return sizeof_array[(uint32_t) ucan_frame];
 	else
 		return 0;
 
 }
 
-uint8_t UCAN_execute_USB_to_CAN_frame(uint8_t *data){
-	UCAN_TxFrameDef* txf = data;
-	UCAN_InitFrameDef* intframe = data;
+static void update_ACK(void) {
+	HAL_FDCAN_GetProtocolStatus(&hfdcan1, &(ack_frame.can_protocol_status));
+	HAL_FDCAN_GetErrorCounters(&hfdcan1, &(ack_frame.can_error_counters));
+	//				memcpy(&ack_frame.can_init_structure, &hfdcan1.Instance,
+	//						sizeof(hfdcan1.Instance));
+}
+
+uint8_t UCAN_execute_USB_to_CAN_frame(uint8_t *data) {
+	UCAN_TxFrameDef *txf = data;
+	UCAN_InitFrameDef *intframe = data;
 	extern FDCAN_HandleTypeDef hfdcan1;
 	if (data == NULL)
-    	return 1;
+		return 1;
 
-	switch(txf->frame_type)
-	{
-		case UCAN_FD_INIT:
-			memcpy(&(hfdcan1.Init),&intframe->can_init , sizeof(FDCAN_InitTypeDef));
-			if (HAL_FDCAN_Init(&hfdcan1) != HAL_OK) {
-					Error_Handler();
-				}
-			HAL_FDCAN_Start(&hfdcan1);
-			HAL_FDCAN_GetProtocolStatus(&hfdcan1,
-					&(ack_frame.can_protocol_status));
-			HAL_FDCAN_GetErrorCounters(&hfdcan1,
-					&(ack_frame.can_error_counters));
-//				memcpy(&ack_frame.can_init_structure, &hfdcan1.Instance,
-//						sizeof(hfdcan1.Instance));
+	switch (txf->frame_type) {
+	case UCAN_FD_INIT:
+		memcpy(&(hfdcan1.Init), &intframe->can_init, sizeof(FDCAN_InitTypeDef));
+		if (HAL_FDCAN_Init(&hfdcan1) != HAL_OK) {
+			Error_Handler();
+		}
+		HAL_FDCAN_Start(&hfdcan1);
+		update_ACK();
+		RING_put(&usb_tx, &ack_frame, sizeof(ack_frame));
+
+		break;
+	case UCAN_FD_DEINIT:
+		//UCAN_Deinit();
+		//add ACK to fifo
+		break;
+	case UCAN_FD_TX:
+		if (HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &(txf->can_tx_header),
+				txf->can_data) == HAL_OK) {
+			//TODO add sth
 			RING_put(&usb_tx, &ack_frame, sizeof(ack_frame));
+		} else {
+			//TODO add sth
+		}
+		break;
+	case UCAN_FD_SAVE_CONFIG:
+		break;
+	case UCAN_FD_GO_TO_BOOTLOADER:
+		//todo: send ack,
+		jump_to_boot();
 
 		break;
-		case UCAN_FD_DEINIT:
-			//UCAN_Deinit();
-			//add ACK to fifo
+	case UCAN_FD_GET_CAN_STATUS:
+		update_ACK();
+		RING_put(&usb_tx, &ack_frame, sizeof(ack_frame));
 		break;
-		case UCAN_FD_TX:
-			if (HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &(txf->can_tx_header), txf->can_data) == HAL_OK)
-			{
-				//TODO add sth
-				RING_put(&usb_tx, &ack_frame, sizeof(ack_frame));
-			} else
-			{
-				//TODO add sth
-			}
-			break;
-		case UCAN_FD_SAVE_CONFIG:
+	case UCAN_FD_RX:
+		//not used
 		break;
-		case UCAN_FD_GO_TO_BOOTLOADER:
-			//todo: send ack,
-			jump_to_boot();
+	case UCAN_FD_ACK:
 
 		break;
-		case UCAN_FD_GET_CAN_STATUS:
 
-		break;
-		case UCAN_FD_RX:
-			//not used
-		break;
-		case UCAN_FD_ACK:
-			// not used
-		break;
-
-		default:
-			return 1;
+	default:
+		return 1;
 	}
 
-    return 0;
+	return 0;
 }
 
 uint8_t UCAN_execute_CAN_to_USB_frame(uint8_t *data, uint32_t len) {
 
 	return 0;
 }
-
 
